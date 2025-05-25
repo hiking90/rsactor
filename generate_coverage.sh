93257f81bdab2e4a7c4a5e24725035f31c64f2e2
Jeff Kim <hiking90@gmail.com>
2025-05-25 16:21:17 +0900

    Refactor coverage generation: remove example runs from generate_coverage.sh and create run_examples.sh for better organization

--- generate_coverage.sh
+++ generate_coverage.sh
@@ -48,16 +48,6 @@ cargo build
 echo "Running tests..."
 cargo test
 
-# Run examples - this also generates .profraw files
-# echo "Running examples..."
-# for example_file in examples/*.rs; do
-#     if [ -f "$example_file" ]; then
-#         example_name=$(basename "$example_file" .rs)
-#         echo "Running example: $example_name"
-#         cargo run --example "$example_name"
-#     fi
-# done
-
 # Generate coverage report using grcov
 echo "Generating coverage report..."
 grcov ./target/coverage/profraws/ \
